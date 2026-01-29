eb1a1bde962a05f250c8879beb3321a175d9ca72
popstarfreas <popstarfreas2@gmail.com>
2026-01-29 19:02:08 +0000

    feat: Update to v145 unlock method

--- app/commands/creativeunlock/index.ts
+++ app/commands/creativeunlock/index.ts
@@ -3,9 +3,9 @@ import Command from "terrariaserver-lite/command";
 import CommandHandler from "terrariaserver-lite/commandhandler";
 import CommandHandlers from "terrariaserver-lite/commandhandlers";
 import CreativeUnlock from "../../index.js";
-import { NetModuleLoadPacket } from "terraria-packet";
+import { NetModuleLoadPacket, PlayerTeamUpdatePacket } from "terraria-packet";
 
-const MAX_ITEM_NET_ID = 5087;
+const MAX_ITEM_NET_ID = 6144;
 
 class CreativeUnlockCommand extends CommandHandler {
     public names = ["creativeunlock", "journeyunlock", "unlockallitems"];
@@ -20,21 +20,40 @@ class CreativeUnlockCommand extends CommandHandler {
 
     public handle(_command: Command, client: Client): void {
         if (!this._creativeUnlock.alreadyUnlocked.has(client)) {
-            for (let i = 0; i <= MAX_ITEM_NET_ID; i++) {
-                const netModuleLoadPacketResult = NetModuleLoadPacket.toBuffer({
-                    TAG: "CreativeUnlocks",
+            const clientId = client.id;
+            if (clientId === undefined) {
+                client.server.logger.error("Client ID is undefined.");
+                return;
+            }
+            let team = PlayerTeamUpdatePacket.toBuffer({
+                playerId: clientId,
+                team: 1,
+            })
+            switch (team.TAG) {
+                case "Ok":
+                    client.sendPacket(team._0);
+                    break;
+                case "Error":
+                    client.server.logger.error(`Error creating team packet: ${team._0}`);
+                    break;
+            }
+            for (let i = 1; i <= MAX_ITEM_NET_ID; i++) {
+                let buf = NetModuleLoadPacket.toBuffer({
+                    TAG: "CreativeUnlocksPlayerReport",
                     _0: {
+                        userId: clientId,
                         itemId: i,
-                        researchedCount: 999
+                        researchedCount: 9999,
                     }
                 })
-
-                if (netModuleLoadPacketResult.TAG == "Error") {
-                    const errorMessage = netModuleLoadPacketResult._0.error.message;
-                    client.server.logger.error(`Error while creating NetModuleLoadPacket for creative unlocks: ${netModuleLoadPacketResult._0.context}; ${errorMessage}`);
-                    return
+                switch (buf.TAG) {
+                    case "Ok":
+                        client.sendPacket(buf._0);
+                        break;
+                    case "Error":
+                        client.server.logger.error(`Error creating creative unlocks player report packet: ${buf._0}`);
+                        break;
                 }
-                client.sendPacket(netModuleLoadPacketResult._0);
             }
             this._creativeUnlock.alreadyUnlocked.add(client);
             client.sendChatMessage("Unlocked all items.", { R: 0, G: 255, B: 0 });
